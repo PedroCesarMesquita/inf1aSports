ebc2994e6a877d0dedc6022ccc2dc4b62838c935
Pedro César (Alface) <pedrocesar1410@gmail.com>
2018-06-12 13:19:35 -0300

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-
-let textoLideEl=document.querySelector('#camisasLide');
-let camisasEl = document.querySelector('#camisas');
-camisasEl.addEventListener("mouseover",function(){
-	textoLideEl.innerHTML = '<p>Responsáveis pela produção dos unifomes afirma que já está na reta final: "Está na reta final".</p>';
-} ); 
-camisasEl.addEventListener("mouseout",function(){
-	textoLideEl.innerHTML = '<p>RETA FINAL!</p>';
-} ); 
