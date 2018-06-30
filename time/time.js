b2a92e4e523e5fbb1c47952b2657a4e7e9f96e6a
Pedro César (Alface) <pedrocesar1410@gmail.com>
2018-06-30 20:51:31 -0300

    Update time.js

--- time/time.js
+++ time/time.js
@@ -3,53 +3,84 @@ let jogadores = [
 	{
 		nome: 'JONATA',
 		num: '1',
-		gols: 0
+		gols: 0,
+		chute: 20,
+		dibre: 30,
+		rataria: 99
 	},
 	{
 		nome: 'CEARA',
 		num: '5',
-		gols: 'alguns'
+		gols: 'alguns',
+		chute: 70,
+		dibre: 60,
+		rataria: 50
 	},
 	{
 		nome: 'PEDRO',
 		num: '7',
-		gols: 'x'
+		gols: 'x',
+		chute: 50,
+		dibre: 65,
+		rataria: 70
 	},
 	{
 		nome: 'ALFACE',
 		num: '9',
-		gols: 1
+		gols: 1,
+		chute: 70,
+		dibre: 99,
+		rataria: 99
 	},
 	{
 		nome: 'FUNECO',
 		num: '10',
-		gols: 'muitos'
+		gols: 'muitos',
+		chute: 99,
+		dibre: 99,
+		rataria: 90
 	},
 	{
 		nome: 'GABINO',
 		num: '11',
-		gols: 'x'
+		gols: 'x',
+		chute: 70,
+		dibre: 70,
+		rataria: 70
 	},
 	{
 		nome: 'SOCO',
 		num: '13',
-		gols: 1
+		gols: 1,
+		chute: 0,
+		dibre: 0,
+		rataria: 0
 	},
 	{
 		nome: 'WAGAO',
 		num: '69',
-		gols: 'bastantes'
+		gols: 'bastantes',
+		chute: 80,
+		dibre: 70,
+		rataria: 80
 	},
 	{
 		nome: 'CHULES',
 		num: '92',
-		gols: 2
+		gols: 2,
+		chute: 80,
+		dibre: 75,
+		rataria: 80
 	}
 ];
 
 let h3El = document.querySelector('h3');
 let fotoEl = document.querySelector('#foto-jogador');
 let golsEl = document.querySelector('#gols');
+let chuteEl = document.querySelector('#chute');
+let dibreEl = document.querySelector('#dibre');
+let ratariaEl = document.querySelector('#rataria');
+let overallEl = document.querySelector('#overall');
 
 let jogadoresEl = document.querySelectorAll('.jogadores');
 
@@ -62,5 +93,9 @@ for(let i = 0; i < jogadoresEl.length; i++) {
 		h3El.innerHTML = jogadores[i].nome + ' <span>' + jogadores[i].num + '</span>';
 		fotoEl.src = '../imgs/' + jogadores[i].nome.toLowerCase() + '.jpg';
 		golsEl.innerText = jogadores[i].gols + (jogadores[i].gols == 1 ? ' gol' : ' gols');
+		chuteEl.innerHTML = 'Chute: ' + jogadores[i].chute;
+		dibreEl.innerHTML = 'Dibre: ' + jogadores[i].dibre;
+		ratariaEl.innerHTML = 'Rataria: ' + jogadores[i].rataria;
+		overallEl.innerHTML = 'Overall: ' + parseInt((jogadores[i].chute + jogadores[i].dibre + jogadores[i].rataria) / 3);
 	});
 }
