1826c13ba0091d798d6f9326d0bc0d7b6ade279e
Pedro César (Alface) <pedrocesar1410@gmail.com>
2018-06-30 22:39:02 -0300

    Update main.js

--- main/main.js
+++ main/main.js
@@ -18,7 +18,7 @@ let noticias = [
 	}
 ];
 
-setInterval(function(){
+setInterval(function() {
 	if(tempo == 5) {
 		mudaImagem(1);
 		tempo = 0;
@@ -32,6 +32,8 @@ function mudaImagem(sentido) {
 		indice = (indice == noticias.length  - 1 ? 0 : indice + 1);
 	else
 		indice = (indice ? indice - 1 : noticias.length + 1);
+
+	tituloEl.innerText = noticias[indice].titulo;
 	noticiasEl.style.backgroundImage = 'url("' + noticias[indice].imagem + '")';
 }
 
